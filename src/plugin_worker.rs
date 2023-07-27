84f5aad8491a83690a05bc64e4a2e4aae5fe2920
aeghn <aeghn@outlook.com>
2023-07-27 22:02:07 +0800

    idle add support

--- src/plugin_worker.rs
+++ src/plugin_worker.rs
@@ -1,7 +1,11 @@
 use std::borrow::Borrow;
+use std::cell::RefCell;
 use std::mem::take;
+use std::rc::Rc;
 use std::sync::{Arc, LockResult, Mutex};
+use std::sync::atomic::{AtomicU8, AtomicUsize, Ordering};
 use std::thread::sleep;
+use std::time::Duration;
 use flume::{Receiver, Sender};
 
 
@@ -9,7 +13,7 @@ use futures::future::{Abortable, AbortHandle};
 use futures::StreamExt;
 use gio::{Cancellable, JoinHandle, Task};
 use gio::prelude::CancellableExt;
-use glib::{BoxedAnyObject, MainContext, PRIORITY_DEFAULT, PRIORITY_DEFAULT_IDLE, PRIORITY_HIGH_IDLE, StaticType, ToValue, Type, Value};
+use glib::{BoxedAnyObject, Continue, idle_add, MainContext, PRIORITY_DEFAULT, PRIORITY_DEFAULT_IDLE, PRIORITY_HIGH_IDLE, StaticType, ToValue, Type, Value};
 use glib::ffi::G_PRIORITY_HIGH_IDLE;
 use glib::value::{FromValue, ValueType};
 use gtk::ResponseType::No;
@@ -17,21 +21,19 @@ use tracing::error;
 use crate::inputbar::InputMessage;
 use crate::inputbar::InputMessage::TextChanged;
 use crate::plugins::{Plugin, PluginResult};
-
-
 use crate::shared::UserInput;
+
 use crate::sidebar::SidebarMsg;
 
 
 pub struct PluginWorker<P: Plugin<R>, R: PluginResult> {
     plugin: Arc<Mutex<P>>,
-    results: Option<R>,
-    cancelable: Option<JoinHandle<()>>,
+    idle_workers: Arc<AtomicU8>,
+    results: Arc<Mutex<Vec<R>>>,
+    cancelable: Option<Cancellable>,
     receiver: async_broadcast::Receiver<Arc<InputMessage>>,
     result_sender: Sender<SidebarMsg>
 }
-
-
 
 impl <P: Plugin<R> + 'static + Send, R: PluginResult + 'static> PluginWorker<P, R> {
     pub fn new(plugin: P,
@@ -39,7 +41,8 @@ impl <P: Plugin<R> + 'static + Send, R: PluginResult + 'static> PluginWorker<P, 
                result_sender: Sender<SidebarMsg>) -> Self {
         PluginWorker {
             plugin: Arc::new(Mutex::new(plugin)),
-            results: None,
+            idle_workers: Arc::new(AtomicU8::new(0)),
+            results: Arc::new(Mutex::new(Vec::<R>::new())),
             cancelable: None,
             receiver: input_receiver,
             result_sender,
@@ -63,28 +66,72 @@ impl <P: Plugin<R> + 'static + Send, R: PluginResult + 'static> PluginWorker<P, 
     async fn loop_recv(&mut self) {
         loop {
             let pn = self.receiver.next().await;
-            let rs = self.result_sender.clone();
             if let Some(msg) = pn {
                 let msg: InputMessage = msg.as_ref().clone();
-                match msg {
-                    TextChanged(input) => {
-                        let plugin_arc = self.plugin.clone();
-                        let ui = UserInput::new(input.as_str());
-                        let jh = gio::spawn_blocking( move || {
-                            let vr = if let Ok(lock) = plugin_arc.lock() {
-                                let ui = UserInput::new(input.as_str());
-                                let result = lock.handle_input(&ui);
-                                let res = result.into_iter().map(|r|
-                                    Box::new(r ) as Box<dyn PluginResult>).collect();
-                                res
-                            } else {
-                                vec![]
-                            };
-                            vr
-                        });
-                        jh.()
+                if let TextChanged(input) = msg {
+                    let plugin_arc = self.plugin.clone();
+                    let ui = UserInput::new(input.as_str());
+                    let cancellable = Cancellable::new();
+
+                    let cancelable_receiver = cancellable.clone();
+                    if let Some(cc) = self.cancelable.replace(cancellable) {
+                        cc.cancel();
                     }
-                    _ => {}
+
+                    let ui_clone = ui.clone();
+                    let jh = gio::spawn_blocking(move || {
+                        let vr = if let Ok(lock) = plugin_arc.lock() {
+                            if cancelable_receiver.is_cancelled() {
+                                return None;
+                            }
+
+                            let result = lock.handle_input(&ui_clone);
+                            if cancelable_receiver.is_cancelled() {
+                                return None;
+                            }
+
+                            Some(result)
+                        } else {
+                            None
+                        };
+
+                        vr
+                    });
+
+                    if let Ok(Some(vec)) = jh.await {
+                        let result_arc = self.results.clone();
+                        {
+                            let mut op = result_arc.lock().unwrap();
+                            *op = vec;
+                        }
+
+                        let idle_worker = self.idle_workers.clone();
+                        if idle_worker.fetch_add(1, Ordering::SeqCst) > 1 {
+                            idle_worker.fetch_sub(1, Ordering::SeqCst);
+                        } else {
+                            let sender = self.result_sender.clone();
+                            idle_add(move || {
+                                match result_arc.lock() {
+                                    Ok(mut vec_guard) => {
+                                        if let Some(vv) = vec_guard.pop() {
+                                            sender.send(SidebarMsg::PluginResult(
+                                                ui.clone(),
+                                                Box::new(vv) as Box<dyn PluginResult>))
+                                                .unwrap();
+                                            Continue(true)
+                                        } else {
+                                            idle_worker.fetch_sub(1, Ordering::SeqCst);
+                                            Continue(false)
+                                        }
+                                    }
+                                    _ => {
+                                        idle_worker.fetch_sub(1, Ordering::SeqCst);
+                                        Continue(false)
+                                    }
+                                }
+                            });
+                        }
+                    }
                 }
             }
         }
--- src/preview.rs
+++ src/preview.rs
@@ -1,34 +1,39 @@
 use std::cell::Ref;
 use flume::Receiver;
-use glib::{BoxedAnyObject, Cast};
+use glib::{BoxedAnyObject, Cast, StrV};
+use gtk::Align::Center;
 use gtk::PolicyType::Never;
+use gtk::prelude::WidgetExt;
 use crate::plugins::PluginResult;
 
 #[derive(Clone)]
 pub struct Preview {
     pub preview_window: gtk::ScrolledWindow,
-    selection_change_receiver: Receiver<BoxedAnyObject>
 }
 
 impl Preview {
-    pub fn new(selection_change_receiver: Receiver<BoxedAnyObject>) -> Self {
+    pub fn new() -> Self {
         let preview_window = gtk::ScrolledWindow::builder()
             .hscrollbar_policy(Never)
+            .css_classes(StrV::from(["preview"]))
             .build();
 
         Preview {
             preview_window,
-            selection_change_receiver,
         }
     }
 
-    pub async fn loop_recv(&self) {
-        let receiver = self.selection_change_receiver.clone();
-        // loop {
-            // if let Ok(bao) = receiver.recv_async().await {
-            //     let preview = bao.borrow::<Box<dyn PluginResult>>();
-            //     self.preview_window.set_child(Some(&preview.preview()));
-            // }
-        // }
+    pub async fn loop_recv(&self, receiver: Receiver<BoxedAnyObject>) {
+        loop {
+            if let Ok(bao) = receiver.recv_async().await {
+                let preview = bao.borrow::<Box<dyn PluginResult>>();
+                let child = preview.preview();
+                child.set_hexpand(true);
+                child.set_vexpand(true);
+                child.set_valign(Center);
+                child.set_halign(Center);
+                self.preview_window.set_child(Some(&child));
+            }
+        }
     }
 }
