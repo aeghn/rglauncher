c3e6a4b7df0e4430e4be230caa3f883215e39101
aeghn <aeghn@outlook.com>
2023-12-05 10:10:05 +0800

    .

--- src/plugins/dict/mod.rs
+++ src/plugins/dict/mod.rs
@@ -1,4 +1,4 @@
-use crate::plugins::{Plugin, PluginResult};
+use crate::plugins::{Plugin, PluginPreview, PluginResult};
 use crate::userinput::UserInput;
 use crate::util::{score_utils, string_utils};
 
@@ -20,20 +20,39 @@ use self::mdx_utils::MDictLookup;
 mod mdx_utils;
 mod mdict;
 
-lazy_static! {
-    static ref PREVIEW: Mutex<Option<Fragile<webkit6::WebView>>> = Mutex::new(None);
+pub struct DictResult {
+    word: String,
+    html: String,
+    pub dict: String,
 }
+
+impl PluginResult for DictResult {
+    fn score(&self) -> i32 {
+        return score_utils::middle(0);
+    }
+
+    fn sidebar_icon_name(&self) -> String {
+        "dictionary".to_string()
+    }
+
+    fn sidebar_label(&self) -> Option<String> {
+        Some(self.word.to_string())
+    }
+
+    fn sidebar_content(&self) -> Option<String> {
+        Some(string_utils::truncate(self.dict.as_str(), 60).to_string())
+    }
+
+    fn on_enter(&self) {
+    }
+}
+
 
 pub struct DictPlugin {
     dir_path: String,
     mdxes: Vec<mdx_utils::MDictMemIndex>
 }
 
-pub struct DictPluginResult {
-    word: String,
-    html: String,
-    pub dict: String,
-}
 
 impl DictPlugin {
     pub fn new(dir_path: &str) -> anyhow::Result<Self, anyhow::Error> {
@@ -59,11 +78,11 @@ impl DictPlugin {
         
     }
 
-    pub fn seek(&self, word: &str) -> Vec<DictPluginResult> {
+    pub fn seek(&self, word: &str) -> Vec<DictResult> {
         self.mdxes.iter()
         .filter_map(|mdx| {
             if let Ok(explain) = mdx.lookup_word(word) {
-                Some(DictPluginResult{
+                Some(DictResult {
                     word: word.to_string(), html: explain, dict: mdx.name.to_string() })
             } else {
                 None
@@ -72,11 +91,11 @@ impl DictPlugin {
         .collect()
     }
 
-    fn cycle_seek(&self, word: &str) -> Vec<DictPluginResult> {
+    fn cycle_seek(&self, word: &str) -> Vec<DictResult> {
         let w = word.trim();
         let seek_res = self.seek(w);
 
-        let mut res: Vec<DictPluginResult> = vec![];
+        let mut res: Vec<DictResult> = vec![];
         for item in seek_res {
             if item.html.starts_with("@@@LINK=") {
                 let w2 = item.html.replace("\r\n\0", "").replace("@@@LINK=", "");
@@ -91,61 +110,48 @@ impl DictPlugin {
     }
 }
 
-impl Plugin<DictPluginResult> for DictPlugin {
-    fn handle_input(&self, user_input: &UserInput) -> Vec<DictPluginResult> {
+impl Plugin<DictResult> for DictPlugin {
+    fn refresh_content(&mut self) {
+
+    }
+
+    fn handle_input(&self, user_input: &UserInput) -> anyhow::Result<Vec<DictResult>> {
         if user_input.input.is_empty() {
-            return vec![];
+            return anyhow::Ok(vec![])
         }
 
-        self.cycle_seek(user_input.input.as_str())
+        anyhow::Ok(self.cycle_seek(user_input.input.as_str()))
     }
 }
 
-impl PluginResult for DictPluginResult {
-    fn get_score(&self) -> i32 {
-        return score_utils::middle(0);
+
+pub struct DictPreview {
+    pub preview: WebView,
+}
+
+impl PluginPreview<DictResult> for DictPreview {
+    fn new() -> Self {
+        let webview = WebView::new();
+        webview.set_vexpand(true);
+        webview.set_hexpand(true);
+        webview.set_can_focus(false);
+
+        if let Some(ucm) = webview.user_content_manager() {
+            ucm.remove_all_style_sheets();
+            let css = include_str!("../../../resources/dict.css");
+            let ss = UserStyleSheet::new(css, AllFrames, User, &[], &[]);
+            ucm.add_style_sheet(&ss);
+        }
+
+        DictPreview {
+            preview: webview
+        }
     }
 
-    fn sidebar_icon_name(&self) -> String {
-        "dictionary".to_string()
-    }
+    fn get_preview(&self, plugin_result: DictResult) -> Widget {
+        let html_content = plugin_result.html.replace("\0", " ");
+        self.preview.load_html(html_content.as_str(), None);
 
-    fn sidebar_label(&self) -> Option<String> {
-        Some(self.word.to_string())
-    }
-
-    fn sidebar_content(&self) -> Option<String> {
-        Some(string_utils::truncate(self.dict.as_str(), 60).to_string())
-    }
-
-    fn preview(&self) -> Widget {
-        let mut guard = PREVIEW.lock().unwrap();
-
-        let wv = guard
-            .get_or_insert_with(|| {
-                let webview = WebView::new();
-                webview.set_vexpand(true);
-                webview.set_hexpand(true);
-                webview.set_can_focus(false);
-
-                if let Some(ucm) = webview.user_content_manager() {
-                    ucm.remove_all_style_sheets();
-                    let css = include_str!("../../../resources/dict.css");
-                    let ss = UserStyleSheet::new(css, AllFrames, User, &[], &[]);
-                    ucm.add_style_sheet(&ss);
-                }
-
-                Fragile::new(webview)
-            })
-            .get();
-
-        let html_content = self.html
-            .replace("\0", " ");
-        wv.load_html(html_content.as_str(), None);
-
-        wv.clone().upcast()
-    }
-
-    fn on_enter(&self) {
+        self.preview.clone().upcast()
     }
 }
--- src/pluginworker.rs
+++ src/pluginworker.rs
@@ -8,9 +8,10 @@ use futures::StreamExt;
 use gio::prelude::CancellableExt;
 use gio::Cancellable;
 use glib::{idle_add, ControlFlow, MainContext};
+use tracing::info;
 
 use crate::inputbar::InputMessage;
-use crate::inputbar::InputMessage::TextChanged;
+use crate::inputbar::InputMessage::{RefreshContent, TextChanged};
 use crate::plugins::{Plugin, PluginResult};
 use crate::userinput::UserInput;
 
@@ -67,70 +68,82 @@ impl<P: Plugin<R> + 'static + Send, R: PluginResult + 'static> PluginWorker<P, R
             let pn = self.receiver.next().await;
             if let Some(msg) = pn {
                 let msg: InputMessage = msg.as_ref().clone();
-                if let TextChanged(input) = msg {
-                    let plugin_arc = self.plugin.clone();
-                    let ui = UserInput::new(input.as_str());
-                    let cancellable = Cancellable::new();
 
-                    let cancelable_receiver = cancellable.clone();
-                    if let Some(cc) = self.cancelable.replace(cancellable) {
-                        cc.cancel();
-                    }
+                match msg {
+                    TextChanged(input) => {
+                        let plugin_arc = self.plugin.clone();
+                        let ui = UserInput::new(input.as_str());
+                        let cancellable = Cancellable::new();
 
-                    let ui_clone = ui.clone();
-                    let jh = gio::spawn_blocking(move || {
-                        let vr = if let Ok(lock) = plugin_arc.lock() {
-                            if cancelable_receiver.is_cancelled() {
-                                return None;
-                            }
-
-                            let result = lock.handle_input(&ui_clone);
-                            if cancelable_receiver.is_cancelled() {
-                                return None;
-                            }
-
-                            Some(result)
-                        } else {
-                            None
-                        };
-
-                        vr
-                    });
-
-                    if let Ok(Some(vec)) = jh.await {
-                        let result_arc = self.results.clone();
-                        {
-                            let mut op = result_arc.lock().unwrap();
-                            *op = vec;
+                        let cancelable_receiver = cancellable.clone();
+                        if let Some(cc) = self.cancelable.replace(cancellable) {
+                            cc.cancel();
                         }
 
-                        let idle_worker = self.idle_workers.clone();
-                        if idle_worker.fetch_add(1, Ordering::SeqCst) > 1 {
-                            idle_worker.fetch_sub(1, Ordering::SeqCst);
-                        } else {
-                            let sender = self.result_sender.clone();
-                            idle_add(move || match result_arc.lock() {
-                                Ok(mut vec_guard) => {
-                                    if let Some(vv) = vec_guard.pop() {
-                                        sender
-                                            .send(SidebarMsg::PluginResult(
-                                                ui.clone(),
-                                                Box::new(vv) as Box<dyn PluginResult>,
-                                            ))
-                                            .unwrap();
-                                        ControlFlow::Continue
-                                    } else {
+                        let ui_clone = ui.clone();
+                        let jh = gio::spawn_blocking(move || {
+                            let vr = if let Ok(lock) = plugin_arc.lock() {
+                                if cancelable_receiver.is_cancelled() {
+                                    return None;
+                                }
+
+                                let result = lock.handle_input(&ui_clone);
+                                if cancelable_receiver.is_cancelled() {
+                                    return None;
+                                }
+
+                                Some(result)
+                            } else {
+                                None
+                            };
+
+                            vr
+                        });
+
+                        if let Ok(Some(Ok(vec))) = jh.await {
+                            let result_arc = self.results.clone();
+                            {
+                                let mut op = result_arc.lock().unwrap();
+                                *op = vec;
+                            }
+
+                            let idle_worker = self.idle_workers.clone();
+                            if idle_worker.fetch_add(1, Ordering::SeqCst) > 1 {
+                                idle_worker.fetch_sub(1, Ordering::SeqCst);
+                            } else {
+                                let sender = self.result_sender.clone();
+                                idle_add(move || match result_arc.lock() {
+                                    Ok(mut vec_guard) => {
+                                        if let Some(vv) = vec_guard.pop() {
+                                            sender.send(SidebarMsg::PluginResult(ui.clone(),
+                                                    Box::new(vv) as Box<dyn PluginResult>,
+                                                ))
+                                                .unwrap();
+                                            ControlFlow::Continue
+                                        } else {
+                                            idle_worker.fetch_sub(1, Ordering::SeqCst);
+                                            ControlFlow::Break
+                                        }
+                                    }
+                                    _ => {
                                         idle_worker.fetch_sub(1, Ordering::SeqCst);
                                         ControlFlow::Break
                                     }
-                                }
-                                _ => {
-                                    idle_worker.fetch_sub(1, Ordering::SeqCst);
-                                    ControlFlow::Break
-                                }
-                            });
+                                });
+                            }
                         }
                     }
+                    RefreshContent => {
+                        info!("begin to refresh windows");
+                        let plugin_arc = self.plugin.clone();
+                        gio::spawn_blocking(move || {
+                            if let Ok(mut lock) = plugin_arc.lock() {
+                                lock.refresh_content();
+                            }
+                        });
+                    }
+
+                    _ => {}
                 }
             }
         }
